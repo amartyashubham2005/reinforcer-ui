5176550db79feae9c44c0c0a166b84c654ccaeaa
Amartya Shubham <amartyashubham2005@gmail.com>
2025-08-01 06:08:57 +0530

    fix: remove breadcrumb from top right

--- src/components/common/PageBreadCrumb.tsx
+++ src/components/common/PageBreadCrumb.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+// import { Link } from "react-router";
 
 interface BreadcrumbProps {
   pageTitle: string;
@@ -13,7 +13,7 @@ const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle }) => {
       >
         {pageTitle}
       </h2>
-      <nav>
+      {/* <nav>
         <ol className="flex items-center gap-1.5">
           <li>
             <Link
@@ -43,7 +43,7 @@ const PageBreadcrumb: React.FC<BreadcrumbProps> = ({ pageTitle }) => {
             {pageTitle}
           </li>
         </ol>
-      </nav>
+      </nav> */}
     </div>
   );
 };
